1dd36df0308322c026b40f1b50ea3b1ca8d40e35
MuindiStephen <stephenmuindi241@gmail.com>
2023-09-29 15:51:58 +0300

    user model

---
+++ demoapi/src/main/java/com/stevemd/demoapi/entity/User.java
@@ -0,0 +1,15 @@
+package com.stevemd.demoapi.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+
+
+    private Integer id;
+    private String name;
+    private String password;
+
+}
