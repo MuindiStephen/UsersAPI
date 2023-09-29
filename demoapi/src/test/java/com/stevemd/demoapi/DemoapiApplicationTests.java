39dd1b467481edfb96750110b726b5bfa2aa390f
MuindiStephen <stephenmuindi241@gmail.com>
2023-09-29 14:13:16 +0300

    initial

---
+++ demoapi/src/test/java/com/stevemd/demoapi/DemoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stevemd.demoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
